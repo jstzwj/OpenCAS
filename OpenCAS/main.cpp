063c92e5be41cea8925572804a767aa9b938d43d
JunWang <1103870790@qq.com>
2018-07-22 18:00:49 +0800

    update

--- OpenCAS/main.cpp
+++ OpenCAS/main.cpp
@@ -5,8 +5,10 @@
 
 int main()
 {
-	opencas::multiprecision::unsigned_integer i(0);
+	opencas::multiprecision::unsigned_integer i(11000000000);
+	opencas::multiprecision::unsigned_integer j(1100234324000);
+	i.add(j);
 	std::cout << i.to_string();
-
+	system("pause");
 	return 0;
 }
--- OpenCAS/multiprecision/integer.hpp
+++ OpenCAS/multiprecision/integer.hpp
@@ -1,5 +1,6 @@
 
 #include <vector>
+#include <algorithm>
 #include <cstdint>
 #include <cmath>
 
@@ -10,6 +11,16 @@ namespace opencas
 	namespace multiprecision
 	{
 #define TWO_POW_32 4294967296
+		
+		inline std::uint32_t fullAdder32(std::uint32_t a, std::uint32_t b, std::uint32_t in_carry, std::uint32_t * out_carry)
+		{
+			std::uint64_t c = static_cast<std::uint64_t>(a) +
+				static_cast<std::uint64_t>(b) +
+				static_cast<std::uint64_t>(in_carry);
+			*out_carry = static_cast<std::uint32_t>(c >> 32);
+			return static_cast<std::uint32_t>(c & 0xFFFFFFFF);
+		}
+
 		class unsigned_integer
 		{
 		public:
@@ -20,44 +31,24 @@ namespace opencas
 
 			unsigned_integer(std::uint64_t v)
 			{
-				std::uint32_t low = v % TWO_POW_32;
-				std::uint32_t high = v / TWO_POW_32;
+				std::uint32_t low = static_cast<std::uint32_t>(v & 0xFFFFFFFF);
+				std::uint32_t high = static_cast<std::uint32_t>((v & 0xFFFFFFFF00000000) >> 32);
 				value.push_back(low);
 				value.push_back(high);
 			}
 
 			void add(const unsigned_integer & other)
 			{
-				std::uint32_t tmp = 0;
+				std::uint32_t in_carry = 0;
 				std::size_t i;
 				for ( i = 0; i < value.size() && i<other.value.size(); ++i)
-				{
-					if (TWO_POW_32 - (value[i] + tmp) <= other.value[i])
-					{
-						value[i] = other.value[i] - (TWO_POW_32 - (value[i] + tmp));
-						tmp = 1;
-					}
-					else
-					{
-						value[i] = other.value[i] + value[i];
-						tmp = 0;
-					}
-				}
+					value[i] = fullAdder32(value[i], other.value[i], in_carry, &in_carry);
 
 				if (i >= value.size())
 				{
 					while (i < value.size())
 					{
-						if (TWO_POW_32 - (value[i] + tmp) <= other.value[i])
-						{
-							value[i] = other.value[i] - (TWO_POW_32 - (value[i] + tmp));
-							tmp = 1;
-						}
-						else
-						{
-							value[i] = other.value[i] + value[i];
-							tmp = 0;
-						}
+						value[i] = fullAdder32(value[i], other.value[i], in_carry, &in_carry);
 						++i;
 					}
 				}
@@ -66,19 +57,9 @@ namespace opencas
 				{
 					while (i < other.value.size())
 					{
-						if (TWO_POW_32 - (value[i] + tmp) <= other.value[i])
-						{
-							value.push_back(other.value[i] - (TWO_POW_32 - (value[i] + tmp)));
-							tmp = 1;
-						}
-						else
-						{
-							value.push_back(other.value[i] + value[i]);
-							tmp = 0;
-						}
+						value[i] = fullAdder32(value[i], other.value[i], in_carry, &in_carry);
 						++i;
 					}
-					++i;
 				}
 			}
 			void subtract(const unsigned_integer & other);
@@ -88,10 +69,19 @@ namespace opencas
 			std::string to_string()
 			{
 				std::string str;
-				for (std::size_t i = value.size() - 1; i >= 0; --i)
+				std::uint64_t num = 0;
+				for (std::size_t i = 0; i < value.size(); ++i)
 				{
-					str += std::to_string(value[i]);
+					num = num * TWO_POW_32 + value[i];
+					while (num > 10)
+					{
+						str += std::to_string(num % 10);
+						num /= 10;
+					}
 				}
+				if (num > 0) str += std::to_string(num);
+
+				std::reverse(str.begin(), str.end());
 				return str;
 			}
 		private:
@@ -119,13 +109,6 @@ namespace opencas
 
 			void add(const integer & other)
 			{
-				if (!this->is_negetive && other.is_negetive)
-					return this->subtract(other);
-
-				if (this->is_negetive && !other.is_negetive)
-					return other.subtract(*this);
-
-				integer result;
 			}
 			void subtract(const integer & other);
 			void product(const integer & other);
